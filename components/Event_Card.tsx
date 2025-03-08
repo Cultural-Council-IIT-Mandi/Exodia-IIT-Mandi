7776ce03c7850496298f2313df84aa7f85f6e213
Kartavya728 <kartavya.xenon@gmail.com>
2025-03-09 00:53:34 +0530

    Fix Events Hover Effects

--- components/Event_Card.tsx
+++ components/Event_Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ui/Card.css';  // Import your CSS file
 import Link from 'next/link';
 
@@ -11,10 +11,15 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ imageUrl, title, description, location, price }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-       <Link href={`/eventDetail/${title}`}>
-         <div className='event-card'>
-            <div className="card border-2 border-yellow-300 rounded-3xl">
+        <Link href={`/eventDetail/${title}`} className="event-card">
+            <div
+                className={`card border-2 border-yellow-300 rounded-3xl transition-transform duration-1000 ease-in-out ${isHovered ? 'rotateY-180' : ''}`}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setTimeout(() => setIsHovered(false), 1000)} // Ensures full animation before reversing
+            >
                 <div className="card-img" style={{ backgroundImage: `url(${imageUrl})` }}></div>
                 <div className="card-body">
                     <span className="bg"></span>
@@ -23,16 +28,14 @@ const Card: React.FC<CardProps> = ({ imageUrl, title, description, location, pri
                     <div className="content flex flex-col justify-center items-center">
                         <h2 className="title">{title}</h2>  {/* Title */}
                         <p className="short-description">{description}</p>  {/* Short description */}
-                        <p className="location">Loc : {location}</p>  {/* Location */}
-                        <p className="price">price : {price}</p>  {/* Price */}
+                        <p className="location">Loc: {location}</p>  {/* Location */}
+                        <p className="price">Price: {price}</p>  {/* Price */}
                         <button className="register-button">Register Now</button>  {/* Register button */}
                     </div>
                 </div>
             </div>
-        </div>
         </Link>
     );
 };
 
 export default Card;
-
