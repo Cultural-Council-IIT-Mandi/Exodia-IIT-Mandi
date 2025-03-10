400967e72e966f39e2936539b5a16f17b513b508
pihu26112005 <piyushkumar26november@gmail.com>
2025-03-10 21:52:48 +0530

    Refactor Section11 component layout for improved responsiveness and readability; remove redundant event data from utils

--- components/Home_Home_Section11.tsx
+++ components/Home_Home_Section11.tsx
@@ -14,10 +14,16 @@ const Section11 = ({ scrollYProgress }: { scrollYProgress: any }) => {
           animate={{ y: [0, 10, 0] }}
           transition={{ duration: 3, repeat: Infinity }}>
           <div className={`border-2 border-white/[0.2] relative  h-screen w-screen  glass rounded-3xl ${aboutNPfont11.className}
-                            flex flex-col justify-center items-center`}>
-            <div className='w-1/2 left-0 text-[3.5rem] absolute px-14'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia fugiat quo non temporibus nobis reprehenderit sunt cum hic, facilis, obcaecati ad eum soluta quam iusto repellat. Provident quibusdam molestiae nihil.</div>
-            <div className="w-[2px] absolute bg-white h-[60%] glass" />
-            <div className='w-1/2 right-0 text-[3.5rem] absolute px-14'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat rem, consectetur et quasi fugit illum saepe nisi dignissimos? Hic veritatis cumque adipisci a, non consectetur id quam necessitatibus iste labore?</div>
+                            flex  justify-center items-center`}>
+            <div className='md:w-1/2 md:left-0 max-md:w-full max-md:h-1/2 max-md:top-0 absolute px-14 flex flex-col items-center max-md:mt-10'>
+              <div className='text-[3.5rem] max-xl:text-[3.1rem] max-lg:text-[2.7rem] max-sm:text-[2.4rem] max-[474px]:text-[2.1rem]'>About Exodia</div>
+              <div className='text-[2.5rem] max-xl:text-[2.1rem] max-lg:text-[1.7rem] max-sm:text-[1.4rem] max-[474px]:text-[1.1rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet totam non, quibusdam sequi reprehenderit hic modi ipsum nostrum commodi eos obcaecati voluptatibus perferendis temporibus mollitia iste. Harum, aliquid pariatur?</div>
+            </div>
+            <div className="md:w-[2px] max-md:h-[2px] max-md:w-[70%] md:h-[60%] absolute bg-white  glass max-md:mb-20  max-[380px]:mb-5" />
+            <div className='md:w-1/2 md:right-0 max-md:w-full max-md:h-1/2 max-md:bottom-0 absolute px-14 max-md:mt-10 flex flex-col items-center'>
+              <div className='text-[3.5rem] max-xl:text-[3.1rem]  max-lg:text-[2.7rem] max-sm:text-[2.4rem] max-[474px]:text-[2.1rem]'>Theme</div>
+              <div className='text-[2.5rem] max-xl:text-[2.1rem] max-lg:text-[1.7rem] max-sm:text-[1.4rem] max-[474px]:text-[1.1rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo amet totam non, quibusdam sequi reprehenderit hic modi ipsum nostrum commodi eos obcaecati voluptatibus perferendis temporibus mollitia iste. Harum, aliquid pariatur?</div>
+            </div>
           </div>
         </motion.div>
       </div>
